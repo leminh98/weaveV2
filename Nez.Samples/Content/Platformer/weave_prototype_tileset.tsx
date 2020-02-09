9555638279c1a5c86c9b344dc23a5025c4259d2e
Mak Jankovksy <mkj7@rice.edu>
2020-02-09 13:02:02 -0600

    Adding prototype tsx and tmx files.

---
+++ Nez.Samples/Content/Platformer/weave_prototype_tileset.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="weave_prototype_tileset" tilewidth="32" tileheight="32" tilecount="40" columns="20">
+ <image source="../../../../Art_Assets/ruins_tile_set.png" width="640" height="64"/>
+ <tile id="1">
+  <objectgroup draworder="index" id="4">
+   <object id="6" x="16.9235" y="18.8762" width="0.0813628"/>
+   <object id="7" x="16.3539" y="15.2148" width="0.0813628"/>
+   <object id="8" x="0.25797" y="-0.0345983" width="31.8213" height="31.8838"/>
+  </objectgroup>
+ </tile>
+</tileset>
