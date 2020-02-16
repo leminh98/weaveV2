64a92c97b24c553e95510af4cf1408e8d4b5fc2a
Minh Le <leminh.ams@gmail.com>
2020-02-15 21:13:35 -0600

    preliminaries for map selection screen

--- Nez.Samples/Scenes/MapSelection/MapSelectionScene.cs
+++ Nez.Samples/Scenes/MapSelection/MapSelectionScene.cs
@@ -25,43 +25,43 @@ namespace Nez.Samples
             for (int i = 0; i < 4; i++)
             {
                 
-                var titleBg = Content.Load<Texture2D>("MapSelection/Map" + i);
+                var titleBg = Content.Load<Texture2D>("MapSelection/Map" + i.ToString());
                 var titleBGEntity = CreateEntity("Map" + i.ToString(), 
                     new Vector2(Screen.Width/4 + ((i % 2) * Screen.Width) /2, Screen.Height/4 + ((i > 1 ? 1 : 0) * Screen.Height) /2));
                 titleBGEntity.AddComponent(new SpriteRenderer(titleBg));
             }
             
-            var buttonStyle = new TextButtonStyle(new PrimitiveDrawable(new Color(78, 91, 98), 10f),
-                new PrimitiveDrawable(new Color(244, 23, 135)), new PrimitiveDrawable(new Color(168, 207, 115)))
-            {
-                DownFontColor = Color.Black
-            };
-            
-            #region Continue button
-            var continueButtonStyle = new TextButtonStyle(new PrimitiveDrawable(Color.Lavender, 0f, 10f),
-                new PrimitiveDrawable(new Color(244, 23, 135)), new PrimitiveDrawable(new Color(168, 207, 115)))
-            {
-                FontColor = Color.Black,
-                DownFontColor = Color.Black
-            };
-            
-            var continueButton = _table.Add(new TextButton("Connect", continueButtonStyle)).SetFillX().SetColspan(12)
-                .SetMinHeight(50).GetElement<TextButton>();
-            continueButton.GetLabel().SetFontScale(2);
-			
-            continueButton.OnClicked += butt =>
-            {
-                // stop all tweens in case any demo scene started some up
-                TweenManager.StopAllTweens();
-                Core.StartSceneTransition(new FadeTransition(() =>
-                {
-                    var scene =  new PlatformerScene();
-                    // scene.AddSceneComponent(networkComponent);
-                    return scene as Scene;
-
-                }));
-            };
-            #endregion
+            // var buttonStyle = new TextButtonStyle(new PrimitiveDrawable(new Color(78, 91, 98), 10f),
+            //     new PrimitiveDrawable(new Color(244, 23, 135)), new PrimitiveDrawable(new Color(168, 207, 115)))
+            // {
+            //     DownFontColor = Color.Black
+            // };
+            //
+            // #region Continue button
+            // var continueButtonStyle = new TextButtonStyle(new PrimitiveDrawable(Color.Lavender, 0f, 10f),
+            //     new PrimitiveDrawable(new Color(244, 23, 135)), new PrimitiveDrawable(new Color(168, 207, 115)))
+            // {
+            //     FontColor = Color.Black,
+            //     DownFontColor = Color.Black
+            // };
+            //
+            // var continueButton = _table.Add(new TextButton("Connect", continueButtonStyle)).SetFillX().SetColspan(12)
+            //     .SetMinHeight(50).GetElement<TextButton>();
+            // continueButton.GetLabel().SetFontScale(2);
+			         //
+            // continueButton.OnClicked += butt =>
+            // {
+            //     // stop all tweens in case any demo scene started some up
+            //     TweenManager.StopAllTweens();
+            //     Core.StartSceneTransition(new FadeTransition(() =>
+            //     {
+            //         var scene =  new PlatformerScene();
+            //         // scene.AddSceneComponent(networkComponent);
+            //         return scene as Scene;
+            //
+            //     }));
+            // };
+            // #endregion
         }
         
         
