ad52f7affe41a6baec2110df3a2073525e04dbbe
Minh Le <leminh.ams@gmail.com>
2020-02-25 23:25:01 -0600

    respawning is somehow working over internet

--- Nez.Samples/Scenes/Platformer/OtherPlayer.cs
+++ Nez.Samples/Scenes/Platformer/OtherPlayer.cs
@@ -182,7 +182,7 @@ namespace Nez.Samples
 							pos.X += 20;
 						
 						var platformerScene = Entity.Scene as PlatformerScene;
-						platformerScene.CreateProjectiles(1, pos, _projectileVelocity * _projDir);
+						platformerScene.CreateProjectiles(1, pos, _projDir);
 					}
 					//
 					// if (_fireBounceInputIsPressed)
