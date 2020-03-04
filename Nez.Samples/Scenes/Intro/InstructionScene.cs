eb6002cbea1bf094f2f6e730fd9e6f5c9b25b9c5
Minh Le <leminh.ams@gmail.com>
2020-03-04 04:06:25 -0600

    Fixed #28; Clean up instruction UI

--- Nez.Samples/Scenes/Intro/InstructionScene.cs
+++ Nez.Samples/Scenes/Intro/InstructionScene.cs
@@ -117,33 +117,6 @@ namespace Nez.Samples.Scenes.Intro
 
             #endregion
 
-            // Canvas = CreateEntity("ui").AddComponent(new UICanvas());
-            // Canvas.IsFullScreen = true;
-            // Canvas.RenderLayer = 100;
-            // _table = Canvas.Stage.AddElement(new Table());
-            // _table.SetFillParent(true).Center();
-            // var titleBg = Content.Load<Texture2D>("Intro/TitleBG");
-            // _table.SetBackground(new SpriteDrawable(titleBg));
-            //
-            // var buttonStyle = new TextButtonStyle(new PrimitiveDrawable(new Color(78, 91, 98), 10f),
-            //     new PrimitiveDrawable(new Color(244, 23, 135)), new PrimitiveDrawable(new Color(168, 207, 115)))
-            // {
-            //     DownFontColor = Color.Black
-            // };
-            //
-            //
-            // button = _table.Add(new TextButton("Proceed...", buttonStyle)).SetFillX()
-            //     .SetMinHeight(50).SetMinWidth(200).GetElement<TextButton>();
-            // button.GetLabel().SetFontScale(2, 2);
-            // button.SetDisabled(true);
-            //
-            // button.OnClicked += butt =>
-            // {
-            //     // stop all tweens in case any demo scene started some up
-            //     TweenManager.StopAllTweens();
-            //     Network.connectPhaseDone = true;
-            //     Core.StartSceneTransition(new FadeTransition(() => Activator.CreateInstance(typeof(CharacterSelectionScene)) as Scene));
-            // };
         }
     }
 }
