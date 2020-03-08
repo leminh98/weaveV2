f1cbe64237e0c9d8d35a9eefac3400879b0d0bb3
Minh Le <leminh.ams@gmail.com>
2020-03-07 23:20:36 -0600

    add missing summer tiles

---
+++ Nez.Samples/bin/Debug/Content/Platformer/randomMaps/forest_summer_tile_set.tsx
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="forest_summer_tile_set" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="forest_summer_tile_set.png" width="320" height="320"/>
+    <tile id="6">
+    <properties>
+    <property name="nez:isSlope" type="bool" value="true"/>
+    <property name="nez:slopeTopLeft" type="int" value="0"/>
+    <property name="nez:slopeTopRight" type="int" value="31"/>
+    </properties>
+    <objectgroup draworder="index">
+    <object id="1" x="0" y="0">
+    <polygon points="0,0 0,16 16,16"/>
+    </object>
+    </objectgroup>
+    </tile>
+    <tile id="7">
+    <properties>
+    <property name="nez:isSlope" type="bool" value="true"/>
+    <property name="nez:slopeTopLeft" type="int" value="31"/>
+    <property name="nez:slopeTopRight" type="int" value="0"/>
+    </properties>
+    <objectgroup draworder="index">
+    <object id="1" x="0" y="0">
+    <polygon points="0,0 0,16 16,16"/>
+    </object>
+    </objectgroup>
+    </tile>
+    <tile id="24">
+    <properties>
+    <property name="nez:isSlope" type="bool" value="true"/>
+    <property name="nez:slopeTopLeft" type="int" value="0"/>
+    <property name="nez:slopeTopRight" type="int" value="15"/>
+    </properties>
+    <objectgroup draworder="index">
+    <object id="1" x="0" y="0">
+    <polygon points="0,0 0,16 16,16"/>
+    </object>
+    </objectgroup>
+    </tile>
+    <tile id="25">
+    <properties>
+    <property name="nez:isSlope" type="bool" value="true"/>
+    <property name="nez:slopeTopLeft" type="int" value="16"/>
+    <property name="nez:slopeTopRight" type="int" value="31"/>
+    </properties>
+    <objectgroup draworder="index">
+    <object id="1" x="0" y="0">
+    <polygon points="0,0 0,16 16,16"/>
+    </object>
+    </objectgroup>
+    </tile>
+    <tile id="26">
+    <properties>
+    <property name="nez:isSlope" type="bool" value="true"/>
+    <property name="nez:slopeTopLeft" type="int" value="31"/>
+    <property name="nez:slopeTopRight" type="int" value="16"/>
+    </properties>
+    <objectgroup draworder="index">
+    <object id="1" x="0" y="0">
+    <polygon points="0,0 0,16 16,16"/>
+    </object>
+    </objectgroup>
+    </tile>
+    <tile id="27">
+    <properties>
+    <property name="nez:isSlope" type="bool" value="true"/>
+    <property name="nez:slopeTopLeft" type="int" value="15"/>
+    <property name="nez:slopeTopRight" type="int" value="0"/>
+    </properties>
+    <objectgroup draworder="index">
+    <object id="1" x="0" y="0">
+    <polygon points="0,0 0,16 16,16"/>
+    </object>
+    </objectgroup>
+    </tile>
+    <tile id="60">
+    <properties>
+    <property name="filewtf" type="file" value=""/>
+    <property name="floaty" type="float" value="0"/>
+    <property name="inty" type="int" value="0"/>
+    <property name="nez:isOneWayPlatform" type="bool" value="true"/>
+    <property name="poop" type="color" value=""/>
+    </properties>
+    <objectgroup draworder="index">
+    <object id="1" x="0" y="0" width="32" height="3"/>
+    </objectgroup>
+    </tile>
+    <tile id="61">
+    <properties>
+    <property name="filewtf" type="file" value=""/>
+    <property name="floaty" type="float" value="0"/>
+    <property name="inty" type="int" value="0"/>
+    <property name="nez:isOneWayPlatform" type="bool" value="true"/>
+    <property name="poop" type="color" value=""/>
+    </properties>
+    <objectgroup draworder="index">
+    <object id="1" x="0" y="0" width="32" height="3"/>
+    </objectgroup>
+    </tile>
+    <tile id="62">
+    <properties>
+    <property name="filewtf" type="file" value=""/>
+    <property name="floaty" type="float" value="0"/>
+    <property name="inty" type="int" value="0"/>
+    <property name="nez:isOneWayPlatform" type="bool" value="true"/>
+    <property name="poop" type="color" value=""/>
+    </properties>
+    <objectgroup draworder="index">
+    <object id="1" x="0" y="0" width="32" height="3"/>
+    </objectgroup>
+    </tile>
+    <tile id="63">
+    <properties>
+    <property name="filewtf" type="file" value=""/>
+    <property name="floaty" type="float" value="0"/>
+    <property name="inty" type="int" value="0"/>
+    <property name="nez:isOneWayPlatform" type="bool" value="true"/>
+    <property name="poop" type="color" value=""/>
+    </properties>
+    <objectgroup draworder="index">
+    <object id="1" x="0" y="0" width="32" height="3"/>
+    </objectgroup>
+    </tile>
+</tileset>
